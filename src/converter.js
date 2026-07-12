15ad011d21f06f37f7c56ef4cdd958a7002d39ac
shivam1108-06 <shivamkotak118@gmail.com>
2026-07-12 11:58:13 +0530

    feat: support multiple length conversions

--- src/converter.js
+++ src/converter.js
@@ -1,11 +1,21 @@
 function convertLength(value, from, to) {
+  if (from === "meter" && to === "kilometer") {
+    return value / 1000;
+  }
 
-    if (from === "meter" && to === "kilometer") {
-        return value / 1000;
-    }
+  if (from === "kilometer" && to === "meter") {
+    return value * 1000;
+  }
 
+  if (from === "foot" && to === "inch") {
+    return value * 12;
+  }
+
+  if (from === "inch" && to === "foot") {
+    return value / 12;
+  }
 }
 
 module.exports = {
-    convertLength
+  convertLength,
 };
--- tests/converter.test.js
+++ tests/converter.test.js
@@ -3,3 +3,15 @@ const { convertLength } = require("../src/converter");
 test("1000 meters should convert to 1 kilometer", () => {
     expect(convertLength(1000, "meter", "kilometer")).toBe(1);
 });
+
+test("1 kilometer should convert to 1000 meters", () => {
+  expect(convertLength(1, "kilometer", "meter")).toBe(1000);
+});
+
+test("1 foot should convert to 12 inches", () => {
+  expect(convertLength(1, "foot", "inch")).toBe(12);
+});
+
+test("12 inches should convert to 1 foot", () => {
+  expect(convertLength(12, "inch", "foot")).toBe(1);
+});
